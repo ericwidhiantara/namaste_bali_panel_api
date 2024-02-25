e595975d9f551c6682cce6930f2593654b876669
eric <nukenin.konoha@gmail.com>
2024-02-25 23:17:30 +0700

    refactor

--- models/message_model.py
+++ models/message_model.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+
+
 class MessageModel(BaseModel):
     sender_id: str
     recipient_id: str
     message: str
     is_read: bool = False
-    
--- test_recipient.py
+++ test_recipient.py
@@ -1,8 +1,11 @@
 import asyncio
+
 import websockets
 
+
 async def send_receive_message():
-    async with websockets.connect('ws://localhost:8000/ws/65db3db2da62410f4af29338/65db2b91677dac933d47ee10') as websocket:
+    async with websockets.connect(
+            'ws://localhost:8000/ws/65db3db2da62410f4af29338/65db2b91677dac933d47ee10') as websocket:
         # Send a message
         await websocket.send("Hello, WebSocket! I'm a message from recipient.")
         print("Message sent.")
@@ -10,5 +13,6 @@ async def send_receive_message():
         # Receive a message
         response = await websocket.recv()
         print("Received message:", response)
+
 
 asyncio.run(send_receive_message())
