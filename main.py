aa50bd2012c3b41dc38316eb11ba11f24f589b1a
eric <nukenin.konoha@gmail.com>
2024-02-26 13:27:49 +0700

    feature: add websocket to check online (not working yet)

--- main.py
+++ main.py
@@ -1,37 +1,93 @@
 import json
+import time
 from typing import List
 
 from fastapi import FastAPI, HTTPException, Form, WebSocket, WebSocketDisconnect
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
 
 from auth import AuthController
 from chat import ChatController
 from models import UserModel, MessageModel
 
 app = FastAPI()
+
 auth_handler = AuthController()
 chat_controller = ChatController()
 
 connections: List[WebSocket] = []
+
+
+class MyMiddleware(BaseHTTPMiddleware):
+    async def dispatch(
+            self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        start_time = time.time()
+        response = await  call_next(request)
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        return response
+
+
+app.add_middleware(MyMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
+
+# Assuming you have a list to track active user connections
+active_users = set()
+
+
+@app.websocket("/ws/{user_id}")
+async def websocket_endpoint_online(websocket: WebSocket):
+    await websocket.accept()
+    print(websocket)
+    # Add the new connection to the list of active users
+    active_users.add(websocket)
+    print("WebSocket connection established.")
+
+    try:
+        while True:
+            # Await data from the client
+            data = await websocket.receive_text()
+            print("Received data:", data)
+            json_data = json.loads(data)
+
+            # Extract the user_id from the received data
+            user_id = json_data.get("user_id")
+
+            if user_id is not None:
+                # Check if the user is online
+                is_online = websocket in active_users
+
+                # Send the online status back to the client
+                response = {"user_id": user_id, "is_online": is_online}
+                await websocket.send_text(json.dumps(response))
+            else:
+                print("Invalid message format:", json_data)
+
+    except WebSocketDisconnect:
+        # Remove the disconnected connection from the list of active users
+        active_users.remove(websocket)
+        print("WebSocket connection closed.")
 
 
 # WebSocket endpoint for chat
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    connections.append(websocket)  # Add the new connection to the list
-    print("ini websocket_endpoint", len(connections))
+@app.websocket("/join/{sender_id}/{recipient_id}")
+async def websocket_endpoint_chat(websocket_chat: WebSocket):
+    await websocket_chat.accept()
+    connections.append(websocket_chat)  # Add the new connection to the list
+    print("ini websocket_chat_endpoint", len(connections))
     try:
         while True:
-            data = await websocket.receive_text()
+            data = await websocket_chat.receive_text()
             print("Received data:", data)
             json_data = json.loads(data)
 
@@ -51,7 +107,7 @@ async def websocket_endpoint(websocket: WebSocket):
                 print("Invalid message format:", json_data)
 
     except WebSocketDisconnect:
-        connections.remove(websocket)  # Remove the disconnected connection from the list
+        connections.remove(websocket_chat)  # Remove the disconnected connection from the list
         # Handle disconnection
         pass
 
@@ -96,7 +152,7 @@ async def login(email: str = Form(...), password: str = Form(...)):
     user.pop("_id", None)
     user.pop("password", None)
 
-    access_token = create_access_token({"sub": user["email"], "user": user})
+    access_token = auth_handler.create_access_token({"sub": user["email"], "user": user})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
