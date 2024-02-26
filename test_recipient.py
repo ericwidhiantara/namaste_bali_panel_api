2f090445d1c741a4885be7cf96ec1ca6859d72d1
eric <nukenin.konoha@gmail.com>
2024-02-26 13:28:09 +0700

    feature: add login page

--- test_recipient.py
+++ test_recipient.py
@@ -5,9 +5,13 @@ import websockets
 
 async def send_receive_message():
     async with websockets.connect(
-            'ws://localhost:8000/ws/65db3db2da62410f4af29338/65db2b91677dac933d47ee10') as websocket:
+            'ws://localhost:8000/join/191e9135-11d2-4053-85c3-2f7477d001f6/cbc48f4b-b00d-47ca-be0a-aa5ca6dc0ce5') as websocket:
         # Send a message
-        await websocket.send("Hello, WebSocket! I'm a message from recipient.")
+        await websocket.send({
+            "sender_id": "191e9135-11d2-4053-85c3-2f7477d001f6",
+            "recipient_id": "cbc48f4b-b00d-47ca-be0a-aa5ca6dc0ce5",
+            "message": "Hello, WebSocket! I'm a message from recipient."
+        })
         print("Message sent.")
 
         # Receive a message
