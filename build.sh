63ff87728e1378c1b8b3bd2d78d7148e6ea565e2
eric <nukenin.konoha@gmail.com>
2024-03-20 14:54:39 +0700

    feature: update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker stop chat_app && docker rm chat_app &&
-docker build -t chat_app . && docker run &&
+docker build -t chat_app . &&
 docker run --name chat_app --env-file .env -p 8181:8000 -d chat_app
