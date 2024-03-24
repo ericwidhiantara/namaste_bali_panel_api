a874347228aa2f963d6b405732199c545c83f4d0
eric <nukenin.konoha@gmail.com>
2024-03-24 18:16:56 +0700

    feature: add save image function to aws

--- app/controller/portfolio_controller.py
+++ app/controller/portfolio_controller.py
@@ -7,12 +7,13 @@ from pymongo import MongoClient
 
 from app.handler.http_handler import CustomHttpException
 from app.models.schemas import FormPortfolioModel, FormEditPortfolioModel, FormDeletePortfolioModel
-from app.utils.helper import save_picture, delete_picture
+from app.utils.helper import save_picture, get_object_url
 
 # Connect to MongoDB
 MONGODB_URL = os.getenv("MONGODB_URL")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
-USER_COLLECTION = "projects"
+COLLECTION = "projects"
+IMAGE_COLLECTION = "project_images"
 
 app = FastAPI()
 
@@ -21,19 +22,40 @@ class PortfolioController:
     def __init__(self):
         self.client = MongoClient(MONGODB_URL)
         self.db = self.client[DATABASE_NAME]
-        self.collection = self.db[USER_COLLECTION]
+        self.collection = self.db[COLLECTION]
+        self.image_collection = self.db[IMAGE_COLLECTION]
 
     async def get_projects(self):
         projects = self.collection.find()
+
+        items = []
+        # Iterate the projects
+        for project in projects:
+            images = self.image_collection.find({"project_id": project["id"]})
+
+            # set the images to the project
+            project["images"] = []
+
+            # iterate the images
+            for image in images:
+                # set the image path to aws s3 url
+                image["image_path"] = get_object_url(image["image_path"])
+                # append the image to the project
+                project["images"].append(image)
+
+            # append the project to the items
+            items.append(project)
+
         print("ini projects", projects)
-        return [project for project in projects]
+
+        return [project for project in items]
 
     async def create_project(self, data: FormPortfolioModel):
         # iterate the data.picture
         uploaded_pictures = []
-        upload_dir = "/projects/"+data.title.lower().replace(" ", "-")
+        upload_dir = "/projects/" + data.title.lower().replace(" ", "-")
         for file in data.images:
-            res = save_picture(upload_dir,file)
+            res = save_picture(upload_dir, file)
             if res == "File not allowed":
                 raise CustomHttpException(
                     status_code=status.HTTP_400_BAD_REQUEST,
@@ -49,14 +71,34 @@ class PortfolioController:
             "description": data.description,
             "date_started": data.date_started,
             "date_finished": data.date_finished,
-            "images": uploaded_pictures,
             "created_at": int(datetime.now().timestamp()),
             "updated_at": int(datetime.now().timestamp()),
         }
-
         # Insert project into MongoDB
         self.collection.insert_one(project)
-        return project
+
+        for image in uploaded_pictures:
+            self.image_collection.insert_one({
+                "id": str(uuid.uuid4()),
+                "project_id": project["id"],
+                "image_path": image
+            })
+
+        # set item from project
+        item = project
+
+        # get the images from mongo
+        images = self.image_collection.find({"project_id": item["id"]})
+        # set the images to the project
+        item["images"] = []
+        # iterate the images
+        for image in images:
+            # set the image path to aws s3 url
+            image["image_path"] = get_object_url(image["image_path"])
+            # append the image to the project
+            item["images"].append(image)
+
+        return item
 
     async def edit_project(self, data: FormEditPortfolioModel):
 
@@ -68,35 +110,54 @@ class PortfolioController:
             )
         # iterate the data.picture
         uploaded_pictures = []
-        upload_dir = "/projects/"+data.title.lower().replace(" ", "-")
+        upload_dir = "/projects/" + data.title.lower().replace(" ", "-")
 
         if data.images is not None:
             for file in data.images:
                 res = save_picture(upload_dir, file)
-                if res == "File not allowed":
+                if res == "File extension not allowed":
                     raise CustomHttpException(
                         status_code=status.HTTP_400_BAD_REQUEST,
-                        message="File not allowed"
+                        message="File extension not allowed"
                     )
                 uploaded_pictures.append(res)
 
         # Create new project
         project = {
-
             "title": data.title if data.title else item["title"],
             "slug": data.title.lower().replace(" ", "-"),
             "description": data.description if data.description else item["description"],
             "date_started": data.date_started if data.date_started else item["date_started"],
             "date_finished": data.date_finished if data.date_finished else item["date_finished"],
-            "images": uploaded_pictures if uploaded_pictures else item["images"],
             "updated_at": int(datetime.now().timestamp()),
         }
+
+        for image in uploaded_pictures:
+            self.image_collection.insert_one({
+                "id": str(uuid.uuid4()),
+                "project_id": data.id,
+                "image_path": image
+            })
 
         print("ini project", project)
         # Update project into MongoDB
         self.collection.update_one({"id": data.id}, {"$set": project})
         updated = self.collection.find_one({"id": data.id})
-        return updated
+
+        # set item from updated data
+        item = updated
+        # get the images from mongo
+        images = self.image_collection.find({"project_id": item["id"]})
+        # set the images to the item
+        item["images"] = []
+        # iterate the images
+        for image in images:
+            # set the image path to aws s3 url
+            image["image_path"] = get_object_url(image["image_path"])
+            # append the image to the item
+            item["images"].append(image)
+
+        return item
 
     async def delete_project(self, data: FormDeletePortfolioModel):
 
@@ -107,14 +168,14 @@ class PortfolioController:
                 message="Project not found"
             )
 
-        if item["images"] is not None:
-            for file in item["images"]:
-                res = delete_picture(file)
-                if not res:
-                    raise CustomHttpException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        message="Failed to delete picture"
-                    )
+        images = self.image_collection.find({"project_id": data.id})
+
+        if images is not None:
+            # print the image
+            for image in images:
+                print("ini file", image)
+                # delete the image
+                self.image_collection.delete_one({"id": image["id"]})
 
         # Delete project from MongoDB
         self.collection.delete_one({"id": data.id})
--- app/utils/helper.py
+++ app/utils/helper.py
@@ -1,5 +1,6 @@
 import os
 
+import boto3
 from fastapi import Form
 
 
@@ -16,6 +17,20 @@ def form_body(cls):
 UPLOAD_DIRECTORY = "uploads"  # Specify the directory where you want to save the uploaded pictures
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}  # Specify the allowed image file extensions
 
+# Set your AWS credentials
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION')
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+
+# Create an S3 client
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_DEFAULT_REGION
+)
+
 
 def allowed_file(file):
     return '.' in file and file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -28,17 +43,46 @@ def save_picture(directory, file):
     if not allowed_file(file.filename):
         return "File extension not allowed"
 
-    if not os.path.exists(UPLOAD_DIRECTORY + directory):
-        os.makedirs(UPLOAD_DIRECTORY+directory, exist_ok=True)
-    file_path = os.path.join(UPLOAD_DIRECTORY+directory, file.filename)
-    print("file path", file_path)
-    with open(file_path, "wb") as f:
-        f.write(file.file.read())
-    return file_path
+    # Generate a unique file name in S3
+    s3_filename = os.path.join(UPLOAD_DIRECTORY + directory, file.filename)
+
+    try:
+        # Upload file to S3
+        s3.upload_fileobj(file.file, AWS_BUCKET_NAME, s3_filename)
+        print("ini object url", get_object_url(s3_filename))
+        return s3_filename
+    except Exception as e:
+        print("Error uploading file to S3:", e)
+        return None
 
 
 def delete_picture(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
+    try:
+        # Extract bucket name and object key from file path
+        bucket_name, object_key = file_path.split('/', 1)
+
+        # Delete the object from S3
+        print("ini bucket", bucket_name)
+        print("ini object key", object_key)
+
+        s3.delete_object(Bucket=bucket_name, Key=object_key)
         return True
-    return False
+    except Exception as e:
+        print("Error deleting file from S3:", e)
+        return False
+
+
+def get_object_url(object_key):
+    """
+    Get the URL of an object in an S3 bucket.
+    """
+    try:
+        url = s3.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': AWS_BUCKET_NAME, 'Key': object_key},
+            ExpiresIn=3600  # URL expiration time in seconds
+        )
+        return url
+    except Exception as e:
+        print("Error generating object URL:", e)
+        return None
