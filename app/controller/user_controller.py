7dc9487f92d4d9fe149d7aa84f9966dbf5e9f15b
eric <nukenin.konoha@gmail.com>
2024-04-28 11:39:47 +0700

    feature: add user controller

---
+++ app/controller/user_controller.py
@@ -0,0 +1,176 @@
+import os
+import uuid
+from datetime import datetime
+
+from fastapi import FastAPI, status
+from pymongo import MongoClient
+
+from app.handler.http_handler import CustomHttpException
+from app.models.users import FormUserModel, FormEditUserModel
+from app.utils.helper import save_picture
+
+# Connect to MongoDB
+MONGODB_URL = os.getenv("MONGODB_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+COLLECTION = "users"
+
+app = FastAPI()
+
+
+class UserController:
+    def __init__(self):
+        self.client = MongoClient(MONGODB_URL)
+        self.db = self.client[DATABASE_NAME]
+        self.collection = self.db[COLLECTION]
+
+    async def get_users(self):
+        users = self.collection.find()
+
+        items = []
+        # Iterate the users
+        for user in users:
+            # append the user to the items
+            items.append(user)
+
+        print("ini users", users)
+
+        return [user for user in items]
+
+    async def get_users_pagination(self, page_number=1, page_size=10, search=None):
+        # Calculate the skip value based on page_number and page_size
+        skip = (page_number - 1) * page_size
+
+        # Fetch users from the database with pagination
+        users = self.collection.find({
+            '$or': [
+                {'first_name': {'$regex': search, '$options': 'i'}},
+                {'last_name': {'$regex': search, '$options': 'i'}},
+                {'username': {'$regex': search, '$options': 'i'}},
+                {'email': {'$regex': search, '$options': 'i'}},
+                {'phone': {'$regex': search, '$options': 'i'}},
+            ]
+        }).skip(skip).limit(page_size).sort('created_at', -1)
+
+        items = []
+        # Iterate the users
+        for project in users:
+            # set the images to the project
+            project.pop("_id")
+
+            # append the project to the items
+            items.append(project)
+            print("items", items)
+
+        # Count total users for pagination
+        total_users = self.collection.count_documents({
+            '$or': [
+                {'first_name': {'$regex': search, '$options': 'i'}},
+                {'last_name': {'$regex': search, '$options': 'i'}},
+                {'username': {'$regex': search, '$options': 'i'}},
+                {'email': {'$regex': search, '$options': 'i'}},
+                {'phone': {'$regex': search, '$options': 'i'}},
+            ]
+        })
+
+        # Calculate total pages
+        total_pages = (total_users + page_size - 1) // page_size
+
+        # Prepare JSON response
+        response = {
+            "page_number": page_number,
+            "page_size": page_size,
+            "total": total_users,
+            "total_pages": total_pages,
+            "users": items
+        }
+
+        print("ini response", response)
+        return response
+
+    async def create_user(self, data: FormUserModel):
+        # iterate the data.image
+        upload_dir = "/users/" + data.first_name.lower().replace(" ", "-") + "_" + data.last_name.lower().replace(" ",
+                                                                                                                  "-")
+
+        picture_path = save_picture(upload_dir, data.picture)
+        if picture_path == "File extension not allowed":
+            raise CustomHttpException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                message="File extension not allowed"
+            )
+
+        # Create new user
+        user = {
+            "id": str(uuid.uuid4()),
+            "first_name": data.first_name,
+            "last_name": data.last_name,
+            "username": data.username,
+            "email": data.email,
+            "password": data.password,
+            "phone": data.phone,
+            "picture": picture_path,
+            "created_at": int(datetime.now().timestamp()),
+            "updated_at": int(datetime.now().timestamp()),
+        }
+        # Insert user into MongoDB
+        self.collection.insert_one(user)
+
+        # set item from user
+        item = user
+
+        return item
+
+    async def edit_user(self, data: FormEditUserModel):
+
+        item = self.collection.find_one({"id": data.id})
+        if not item:
+            raise CustomHttpException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                message="User not found"
+            )
+
+        upload_dir = "/users/" + data.first_name.lower().replace(" ", "-") + "_" + data.last_name.lower().replace(" ",
+                                                                                                                  "-")
+
+        # Save picture
+        picture_path = save_picture(upload_dir, data.picture)
+        if picture_path == "File extension not allowed":
+            raise CustomHttpException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                message="File extension not allowed"
+            )
+
+        # Create new user
+        user = {
+            "first_name": data.first_name if data.first_name else item["first_name"],
+            "last_name": data.last_name if data.last_name else item["last_name"],
+            "username": data.username if data.username else item["username"],
+            "email": data.email if data.email else item["email"],
+            "phone": data.phone if data.phone else item["phone"],
+            "picture": picture_path if picture_path else item["picture"],
+            "created_at": int(datetime.now().timestamp()),
+            "updated_at": int(datetime.now().timestamp()),
+        }
+
+        print("ini user", user)
+        # Update user into MongoDB
+        self.collection.update_one({"id": data.id}, {"$set": user})
+        updated = self.collection.find_one({"id": data.id})
+
+        # set item from updated data
+        item = updated
+
+        return item
+
+    async def delete_user(self, user_id: str):
+
+        item = self.collection.find_one({"id": user_id})
+        if not item:
+            raise CustomHttpException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                message="User not found"
+            )
+
+        # Delete user from MongoDB
+        self.collection.delete_one({"id": user_id})
+        return None
