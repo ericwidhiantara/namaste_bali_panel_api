c80fa47acce9b8c6ad67df1cf142782fdd1685be
eric <nukenin.konoha@gmail.com>
2024-04-28 11:39:42 +0700

    feature: add user models pagination and edit

--- app/models/users.py
+++ app/models/users.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import Form, UploadFile
 from pydantic import BaseModel, EmailStr
 
@@ -14,6 +16,29 @@ class FormUserModel:
             phone: str = Form(..., description="user phone number"),
             picture: UploadFile = Form(..., description="user picture"),
     ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.password = password
+        self.phone = phone
+        self.picture = picture
+
+
+class FormEditUserModel:
+
+    def __init__(
+            self,
+            id: str = Form(..., description="user id"),
+            first_name: str = Form(..., description="user first name"),
+            last_name: str = Form(..., description="user last name"),
+            username: str = Form(..., description="user username"),
+            email: EmailStr = Form(..., description="user email"),
+            password: str = Form(..., min_length=6, max_length=24, description="user password"),
+            phone: str = Form(..., description="user phone number"),
+            picture: UploadFile = Form(..., description="user picture"),
+    ):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
@@ -44,3 +69,11 @@ class TokenPayload(BaseModel):
     sub: str = None
     exp: int = None
     user: UserModel = None
+
+
+class UserPaginationModel(BaseModel):
+    page_number: int
+    page_size: int
+    total: int
+    total_pages: int
+    users: List[UserModel]
