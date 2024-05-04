a61262e128f731962e9f7055ac0daf8b3d19bc8d
eric <nukenin.konoha@gmail.com>
2024-05-04 19:56:00 +0700

    feature: update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_panel_api$'; then
     sudo docker stop namaste_bali_panel_api && sudo docker rm namaste_bali_panel_api &&
     sudo docker build -t namaste_bali_panel_api . &&
-    sudo docker run --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
+    sudo docker run -v ./uploads:/uploads --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
 else
        sudo docker build -t namaste_bali_panel_api . &&
-       sudo docker run --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
+           sudo docker run -v ./uploads:/uploads --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
 fi
