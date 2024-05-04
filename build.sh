8ea46fce70e7a1cf54c1d12907d5e65857db35a1
eric <nukenin.konoha@gmail.com>
2024-05-04 14:48:21 +0700

    feature: update build.sh file

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-#docker stop chat_app && docker rm chat_app &&
-docker build -t chat_app . &&
-docker run --name chat_app --env-file .env -p 8181:8000 -d chat_app
+if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_panel_api$'; then
+    sudo docker stop namaste_bali_panel_api && sudo docker rm namaste_bali_panel_api &&
+    sudo npm install && sudo npm run build &&
+    sudo docker build -t namaste_bali_panel_api . &&
+    sudo docker run --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
+else
+   sudo npm install && sudo npm run build &&
+       sudo docker build -t namaste_bali_panel_api . &&
+       sudo docker run --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
+fi
