a64470c975c8cd543db71ef918c58180e85d74f3
eric <nukenin.konoha@gmail.com>
2024-05-04 15:38:25 +0700

    feature: remove the pip install

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_panel_api$'; then
     sudo docker stop namaste_bali_panel_api && sudo docker rm namaste_bali_panel_api &&
-    sudo pip install -r requirements.txt &&
     sudo docker build -t namaste_bali_panel_api . &&
     sudo docker run --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
 else
-    sudo pip install -r requirements.txt &&
        sudo docker build -t namaste_bali_panel_api . &&
        sudo docker run --name namaste_bali_panel_api --env-file .env -p 8282:8080 -d namaste_bali_panel_api
 fi
